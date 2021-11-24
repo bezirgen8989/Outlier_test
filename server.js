904d7606d1eb3a0046ac2799a4760b00dfc5b6c5
bezirgen8989 <bezirgen8989@mail.ru>
2021-11-24 16:29:54 +0300

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url === '/base64') return getBase64(req, res)
+<<<<<<< HEAD
   if (req.url === '/user-agent') return getUserAgent(req, res)
 
+=======
+>>>>>>> e52b587 (add base64 endpoint)
   res.end()
 })
 
@@ -18,12 +21,15 @@ function getBase64 (req, res) {
   res.end(JSON.stringify({ base: 'base64' }))
 }
 
+<<<<<<< HEAD
 function getUserAgent (req, res) {
   res.end(JSON.stringify({ userAgentName: 'testUserAgent', id: 44 }))
 }
 
 
 
+=======
+>>>>>>> e52b587 (add base64 endpoint)
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
 
