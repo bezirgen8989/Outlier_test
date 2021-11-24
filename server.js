19b599eefa4b7955d3821ad1bac9b7dc20646e1d
bezirgen8989 <bezirgen8989@mail.ru>
2021-11-24 16:08:05 +0300

    add user-agent endpoint

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/base64') return getBase64(req, res)
+  if (req.url === '/user-agent') return getUserAgent(req, res)
 
   res.end()
 })
@@ -11,6 +13,16 @@ const server = http.createServer((req, res) => {
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
+
+function getBase64 (req, res) {
+  res.end(JSON.stringify({ base: 'base64' }))
+}
+
+function getUserAgent (req, res) {
+  res.end(JSON.stringify({ userAgentName: 'testUserAgent', id: 44 }))
+}
+
+
 
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
