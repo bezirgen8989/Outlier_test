e52b58745b0fe67820754f17d36290f60abd69de
bezirgen8989 <bezirgen8989@mail.ru>
2021-11-24 16:11:37 +0300

    add base64 endpoint

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
-
+  if (req.url === '/base64') return getBase64(req, res)
   res.end()
 })
 
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
+}
+
+function getBase64 (req, res) {
+  res.end(JSON.stringify({ base: 'base64' }))
 }
 
 server.listen(PORT)
